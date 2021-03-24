f5ad2fea9341277e7082f62fda4861c420763f09
LoveSy <shana@zju.edu.cn>
2021-03-24 14:19:31 +0800

    Remove debug log in relase build

--- remote/src/main/cpp/log.h
+++ remote/src/main/cpp/log.h
@@ -5,6 +5,10 @@
 #define TAG "InternalBrowserRedirect"
 
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO  ,TAG ,__VA_ARGS__)
+#ifndef NDEBUG
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG ,TAG ,__VA_ARGS__)
+#else
+#define LOGD(...)
+#endif
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR ,TAG ,__VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN  ,TAG ,__VA_ARGS__)

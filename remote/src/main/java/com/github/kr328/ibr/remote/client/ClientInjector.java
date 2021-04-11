7a7d27c77aa0429164c748c4ee0b6e02ecf64221
LoveSy <shana@zju.edu.cn>
2021-04-11 14:43:38 +0800

    Bypass hidden api

--- remote/src/main/java/com/github/kr328/ibr/remote/client/ClientInjector.java
+++ remote/src/main/java/com/github/kr328/ibr/remote/client/ClientInjector.java
@@ -1,11 +1,21 @@
 package com.github.kr328.ibr.remote.client;
 
+import android.os.Build;
 import android.os.IBinder;
 
 import com.github.kr328.ibr.remote.proxy.ServiceManagerProxy;
 
+import org.lsposed.hiddenapibypass.HiddenApiBypass;
+
 public class ClientInjector {
     public static void inject() throws ReflectiveOperationException {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            HiddenApiBypass.setHiddenApiExemptions(
+                    "Landroid/app/IActivityTaskManager",
+                    "Landroid/app/IActivityManager",
+                    "Landroid/app/IApplicationThread"
+            );
+        }
         final ClientActivityManagerProxy proxy = new ClientActivityManagerProxy();
 
         ServiceManagerProxy.install(new ServiceManagerProxy.Callback() {
